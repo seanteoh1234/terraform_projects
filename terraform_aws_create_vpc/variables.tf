0e40871e8f2c5d86d8b02f69ee40f35941116f88
dillic <92786434+seanteoh1234@users.noreply.github.com>
2024-10-12 00:40:18 +0800

    Update variables.tf
    Removed default value so that an input is required when running terraform apply.

--- terraform_aws_create_vpc/variables.tf
+++ terraform_aws_create_vpc/variables.tf
@@ -1,11 +1,9 @@
 variable "region" {
   description = "A set of key/value label pairs to assign to the bucket."
   type        = string
-  default     = null
 }
 
 variable "availability_zone" {
   description = "A set of key/value label pairs to assign to the bucket."
   type        = string
-  default     = null
 }
